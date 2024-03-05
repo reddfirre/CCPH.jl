d2bbc6418f496badeecf9d834aac35c1f186e209
PeterFransson <peter.fransson@gmail.com>
2024-03-05 13:19:59 +0100

    new ψ₅₀ value

--- src/ModelStructs.jl
+++ src/ModelStructs.jl
@@ -166,7 +166,7 @@ mutable struct HydraulicsPar{T<:Float64}
     Kₓₗ₀::T #Maximum xylem-leaf hydraulic conductance (mol m⁻² leaf s⁻¹ MPa⁻¹)     
 end
 #Standard values
-HydraulicsPar(;ψ₅₀::T=-2.89,b::T=2.15,i::T=1.0,Kₓₗ₀::T=0.01) where {T<:Float64} = 
+HydraulicsPar(;ψ₅₀::T=-2.7,b::T=2.15,i::T=1.0,Kₓₗ₀::T=0.01) where {T<:Float64} = 
 HydraulicsPar(ψ₅₀,b,i,Kₓₗ₀)
 
 #Collection of structs used for the instantaneous Photosynthesis and Hydraulic model
